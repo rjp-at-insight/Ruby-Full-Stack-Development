338124a1a6a1113459d8932d5ba31ad51cf72187
rjp-at-insight <robert.patterson@insight.com>
2020-10-03 16:01:11 -0400

    fixed missing filles

---
+++ Flow-Control-and-Data-Types/encode.rb
@@ -0,0 +1,8 @@
+#   AUTHOR      : Robert James Patterson
+#   DATE        : 10/03/2020
+#   FILE        : encode.rb
+#   SYNOPSIS    : Work-thru file for 'Ruby Full Stack Development: Flow Control & Data Types'.
+#
+
+# encoding: US-ASCII
+puts __ENCODING__
---
+++ Flow-Control-and-Data-Types/encode2.rb
@@ -0,0 +1,10 @@
+#   AUTHOR      : Robert James Patterson
+#   DATE        : 10/03/2020
+#   FILE        : encode2.rb
+#   SYNOPSIS    : Work-thru file for 'Ruby Full Stack Development: Flow Control & Data Types'.
+#
+
+str = "Howdy, bitches!"
+puts str.encoding
+puts str.encode!("US-ASCII")
+puts str.encoding
