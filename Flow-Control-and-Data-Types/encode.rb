65fb15ea4af75b55541d7cab2c4f9cada3ca70ef
rjp-at-insight <robert.patterson@insight.com>
2020-10-03 16:46:03 -0400

    updated encode.rb

--- Flow-Control-and-Data-Types/encode.rb
+++ Flow-Control-and-Data-Types/encode.rb
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 #   AUTHOR      : Robert James Patterson
 #   DATE        : 10/03/2020
 #   FILE        : encode.rb
 #   SYNOPSIS    : Work-thru file for 'Ruby Full Stack Development: Flow Control & Data Types'.
 #
 
-# encoding: US-ASCII
-=======
->>>>>>> 579a59fe340c575c2bb52c63f21117babf94534e
+
 puts __ENCODING__
