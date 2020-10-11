405b4e5813b7a133d13f526b1e01c971cfe72247
rjp-at-insight <robert.patterson@insight.com>
2020-10-11 12:30:09 -0400

    updated fcdt-final

--- Flow-Control-and-Data-Types/fcdt-final.rb
+++ Flow-Control-and-Data-Types/fcdt-final.rb
@@ -7,11 +7,16 @@
 
 # Create a loop that displays 1 to 5
 i = 1
-
+puts "Using \'While\' to loop five times."
 while i <= 5
     printf("%d\n", i)
     i += 1
 end 
+
+# now using until
+i = 0
+puts "Using \'Until\' to loop five times."
+puts i += 1 until i >= 5
 
 # Use a hash to store grades for twu students
 grades = {"Susan" => "100", "Robert" => "97"}
@@ -21,7 +26,7 @@ printf("Robert scored a : %s\n", grades.fetch('Robert', 'N/A'))
  
 # Use an array to store 3 numbers
 vals = (1..3).to_a
-printf("\nInitial Array\n")
+puts "Initial Array")
 puts vals
 
 # insert a value to the beginning of an array
